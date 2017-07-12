b843e002a9b5b6856d983430e44a21e2126b37d2
Ram Moharil <rammoharil@gmail.com>
2017-07-12 20:11:29 +0530

    Ionic 2 application with sqlite db calls

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,Platform } from 'ionic-angular';
+import {SQLite} from 'ionic-native';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+firstName:string;
+ lastName:string;
+  constructor(public navCtrl: NavController,platform: Platform) {
+    platform.ready().then(() => {
+            
+        });
+  }
+ 
+  public SubmitData()
+  {
+    //Validation for mandatory fields
+    if(typeof this.firstName == 'undefined' || this.firstName == null || this.firstName=='')
+    {
+      alert('Please enter the first name.');
+      return false;
+    }
+    if(typeof this.lastName == 'undefined' || this.lastName == null || this.lastName=='')
+    {
+      alert('Please enter the last name.');
+      return false;
+    } 
 
-  constructor(public navCtrl: NavController) {
-
+    //Starting database operations
+    let db = new SQLite();
+    let query = "INSERT INTO people (firstname, lastname) VALUES ('"+ this.firstName+"', '"+this.lastName +"')";
+    
+            db.openDatabase({name: "data.db", location: "default"}).then(() => {
+                db.executeSql(query, []).then((data) => {
+            console.log("INSERTED: " + JSON.stringify(data));
+            alert('User has been registered successfully.');
+        }, (error) => {
+            console.log("ERROR: " + JSON.stringify(error.err)); //error while executing query
+        });
+            }, (error) => {
+                console.log("ERROR: ", error); //error while opening database
+            });
+    
   }
 
 }
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {SQLite} from 'ionic-native';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
+people:any;
   constructor(public navCtrl: NavController) {
-
+    this.refresh();
   }
+
+  public refresh() {
+    let db = new SQLite();
+            db.openDatabase({name: "data.db", location: "default"}).then(() => {
+                db.executeSql("SELECT * FROM people", []).then((data) => {
+            this.people = [];
+            if(data.rows.length > 0) {
+                for(var i = 0; i < data.rows.length; i++) {
+                    this.people.push({firstname: data.rows.item(i).firstname, lastname: data.rows.item(i).lastname});
+                }
+            }
+        }, (error) => {
+            console.log("ERROR: " + JSON.stringify(error));
+        });
+            }, (error) => {
+                console.log("ERROR: ", error);
+            });
+
+    }
 
 }
